26edc647e4fb57e0852392b5df3820f9289efa97
lyudmila.gold <f.lyudmila1988@gmail.com>
2024-11-16 20:55:40 +0300

    Add files via upload

---
+++ ДЗ - №5.py
@@ -0,0 +1,9 @@
+example = 'Lyudmila'
+print(example[0])
+print(example[-1])
+second_half = example[len(example)//2:]
+print(second_half)
+reversed_example = example[::-1]
+print(reversed_example)
+every_second_char = example[1::2]
+print(every_second_char)
