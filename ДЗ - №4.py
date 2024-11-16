11f383e06cba77875237276054a2dc9de8416b3a
lyudmila.gold <f.lyudmila1988@gmail.com>
2024-11-16 18:29:13 +0300

    Add files via upload

---
+++ ДЗ - №4.py
@@ -0,0 +1,7 @@
+completed_tasks = 12
+spent_hours = 1.5
+course_name = 'Python'
+average_time_per_task = spent_hours / completed_tasks
+print('Курс:', course_name + ',', 'всего задач:', completed_tasks,
+      'затрачено часов:', spent_hours,
+      'среднее время выполнения', round(average_time_per_task, 3), 'часа.')
