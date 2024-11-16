7ddf50ab50dc6ec1a7072c3eaddccb0f422fd625
lyudmila.gold <f.lyudmila1988@gmail.com>
2024-11-16 17:56:15 +0300

    Add files via upload

---
+++ ДЗ - №3.py
@@ -0,0 +1,10 @@
+name = "Lyudmila"
+print (name, type(name))
+age = 36
+print("Age:", age)
+new_age = 1
+age += new_age
+print("New Age:", age)
+is_student = True
+print("Is Student:", is_student)
+
